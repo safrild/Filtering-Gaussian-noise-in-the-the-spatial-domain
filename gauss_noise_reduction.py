750e9d10355df2c8d317ebae041d530d9250a3ab
Safrany Ildiko Csillag <safrild09@gmail.com>
2020-09-28 17:19:31 +0200

    Kuwahara smallest deviation, mean

--- gauss_noise_reduction.py
+++ gauss_noise_reduction.py
@@ -1,5 +1,6 @@
 import cv2 as cv2
 import numpy as np
+import statistics
 
 # kepek beolvasasa es tombbe helyezese
 im = cv2.imread('im1.jpg', cv2.IMREAD_GRAYSCALE)
@@ -41,23 +42,82 @@ def kuwahara(img):
     cv2.imshow('Photo after noising', imnoise)
 
     # Letrehozunk egy 5x5-os kernelt
-    kernel = np.ones((5, 5), np.uint8)
+    # kernel = np.ones((5, 5), np.uint8)
 
     # a szeleket levesszuk egyelore
     # inner = [400, 400, 600, 600]
-    # rect = cv2.rectangle(imnoise1, (inner[0], inner[1]), (inner[2], inner[3]), 0, 1)
+    # rect = cv2.rectangle(imnoise, (inner[0], inner[1]), (inner[2], inner[3]), 0, 1)
     # mean = cv2.mean(rect)
 
-    # mean = cv2.mean(imnoise1, kernel)
+    # mean = cv2.mean(imnoise, kernel)
     # print(mean)
 
     rows, cols = imnoise.shape
-    counter = 0
-    for i in range(rows):
-        for j in range(cols):
+    # print("kepmeret:", imnoise.shape)
+
+    for i in range(0, rows):
+        for j in range(0, cols):
             current_pixel = imnoise[i, j]
-            counter = counter + 1
-    print(counter)
+            # print("aktualis pixel es i es j: ", current_pixel, i, j)
+            # kihagyjuk a kep szeleit egyelore
+            if j >= cols - 2 or i >= rows - 2:
+                break
+
+            # kernel[2, 2] = current_pixel
+            # https://stackoverflow.com/questions/28591549/sliding-window-over-an-image-opencv
+            # https://stackoverflow.com/questions/10896841/find-a-3x3-sliding-window-over-an-image?answertab=active#tab-top
+            # https://www.pyimagesearch.com/2015/03/23/sliding-windows-for-object-detection-with-python-and-opencv/
+            Q1 = [imnoise[i - 2, j - 2], imnoise[i - 2, j - 1], imnoise[i - 2, j],
+                  imnoise[i - 1, j - 2], imnoise[i - 1, j - 1], imnoise[i - 1, j],
+                  imnoise[i, j - 2], imnoise[i, j - 1], imnoise[i, j]]
+            Q2 = [imnoise[i - 2, j], imnoise[i - 2, j + 1], imnoise[i - 2, j + 2],
+                  imnoise[i - 1, j], imnoise[i - 1, j + 1], imnoise[i - 1, j + 2],
+                  imnoise[i, j], imnoise[i, j + 1], imnoise[i, j + 2]]
+            Q3 = [imnoise[i, j - 2], imnoise[i, j - 1], imnoise[i, j],
+                  imnoise[i + 1, j - 2], imnoise[i + 1, j + 1], imnoise[i + 1, j],
+                  imnoise[i + 2, j - 2], imnoise[i + 2, j - 1], imnoise[i + 2, j]]
+            Q4 = [imnoise[i, j], imnoise[i, j + 1], imnoise[i, j + 2],
+                  imnoise[i + 1, j], imnoise[i + 1, j + 1], imnoise[i + 1, j + 2],
+                  imnoise[i + 2, j], imnoise[i + 2, j + 1], imnoise[i + 2, j + 2]]
+
+            # print(Q1)
+            # print(cv2.mean(np.int32(Q1))[0])
+            # print(statistics.stdev(np.int32(Q1)))
+
+            meanq1 = round(cv2.mean(np.int32(Q1))[0], 4)
+            meanq2 = round(cv2.mean(np.int32(Q2))[0], 4)
+            meanq3 = round(cv2.mean(np.int32(Q3))[0], 4)
+            meanq4 = round(cv2.mean(np.int32(Q4))[0], 4)
+
+            devq1 = round(statistics.stdev(np.int32(Q1)), 4)
+            devq2 = round(statistics.stdev(np.int32(Q2)), 4)
+            devq3 = round(statistics.stdev(np.int32(Q3)), 4)
+            devq4 = round(statistics.stdev(np.int32(Q4)), 4)
+
+            mean = {
+                'Q1': meanq1,
+                'Q2': meanq2,
+                'Q3': meanq3,
+                'Q4': meanq4,
+            }
+
+            deviation = {
+                'Q1': devq1,
+                'Q2': devq2,
+                'Q3': devq3,
+                'Q4': devq4
+            }
+
+            print('Deviations:', deviation)
+            smallestdevvalue = min(deviation.values())
+            smallestdevregion = min(deviation, key=deviation.get)
+            print('Smallest deviation region: ', smallestdevregion)
+            # meanofregion = [r for r, m in mean.items() if r == smallestdevregion]
+            meanofregion = mean[smallestdevregion]
+            print('Means: ', mean)
+            print('Mean of region with smallest dev: ', meanofregion)
+
+            # current_pixel = mean.get()
 
     return imnoise
 
